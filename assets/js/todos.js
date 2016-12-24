2e817a2e582549217f5afa78a712bc361c73535a
Christopher Wilson <cmw.wilson88@gmail.com>
2016-12-24 17:32:18 -0500

    Third video changes.  Add functionality to delete individual todos.  lesson focused on stopPropagation(), then grabbing the parent() of span, then adding anon function in fadeOut() to remove li from DOM after fadeOut is completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").on("click", function() {
 	$(this).toggleClass("completed");
 });
+
+
+//click on x to delete todo
+$("span").on("click", function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
